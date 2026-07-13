270799ebc5cf25b2743c130b6975d7d353b5bac3
Aryan Kasera <kaseraaryan5@gmail.com>
2026-07-13 17:52:58 +0530

    My code

---
+++ Day1.py
@@ -0,0 +1,17 @@
+print("Hello World")
+name = "Aryan Kasera"
+print(name)
+print(type(name))
+age = 18 
+print(age)
+print(type(age))
+price = 100.0
+print(price)
+print(type(price))
+old = True
+print(old)
+print(type(old))
+a = None
+print(a)
+print(type(a))
+
---
+++ Day2.py
@@ -0,0 +1,10 @@
+name = "Aryan Kasera"
+input("name")
+age = 18
+input(age)
+price = 100.0
+input(price)
+old = True
+input(old)
+a = None
+input(a)
