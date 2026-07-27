4c1f9f1a18b7e6f6dbc19b2b551a639a955fc7b8
Aryan Kasera <kaseraaryan5@gmail.com>
2026-07-27 10:55:09 +0530

    Added master logger and exception setup in src

---
+++ src/logger.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+# 1. लॉगिंग सेटअप (कृश नाइक सर वाला लॉजिक)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger("AI_Project")
+
+
+# 2. कस्टम एक्सेप्शन हैंडलिंग सेटअप (मास्टर टूल)
+# यह फंक्शन अपने आप पता लगाएगा कि एरर किस फ़ाइल में और किस लाइन नंबर पर आया है
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occurred in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
