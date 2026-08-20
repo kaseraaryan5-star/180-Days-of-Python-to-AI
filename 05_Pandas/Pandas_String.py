0b7c4b82b0971e18099709480a938e94183658ad
Aryan Kasera <kaseraaryan5@gmail.com>
2026-08-20 09:16:52 +0530

    Pandas String Complete

--- 05_Pandas/Pandas_String.py
+++ 05_Pandas/Pandas_String.py
@@ -6,8 +6,8 @@ a = np.array([1,2,3,4])
 print(a)
 
 #problem in vectorized operation in vanilla python
-s = ['cat','mat',None,'rat']
-[i.startswith('c') for i in s]
+#s = ['cat','mat',None,'rat']
+#[i.startswith('c') for i in s]
 
 #How pandas solve this issue?
 s = pd.Series(['cat','mat',None,'rat'])
@@ -48,3 +48,29 @@ print(df['title'].value_counts())
 df['title'] = df['title'].str.replace('Ms.','Miss.')
 df['title'] = df['title'].str.replace('Mlle.','Miss.')
 print(df['title'].value_counts())
+
+
+
+#Filtering
+#startwith/endswith
+print(df[df['firstname'].str.endswith('A')])
+print(df[df['firstname'].str.startswith('A')])
+
+#isdigit/isalpha
+print(df[df['firstname'].str.isdigit()])
+print(df[df['firstname'].str.isalpha()])
+
+
+
+#Applying regex
+#contains
+#search john -> both case
+print(df[df['firstname'].str.contains('john',case=False)])
+
+#find last name with starts and ends char vowel
+print(df[df['lastname'].str.contains('^[aeiouAEIOU].+[aeiouAEIOU]$')])
+
+
+
+#slicing
+print(df['Name'].str[::2])
