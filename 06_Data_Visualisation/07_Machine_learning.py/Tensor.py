878999cb67cfac373842dd35022907465d486d3b
Aryan Kasera <kaseraaryan5@gmail.com>
2026-09-02 18:10:55 +0530

    What is Tensor

---
+++ 06_Data_Visualisation/07_Machine_learning.py/Tensor.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array(4)
+print(a)
+print(a.ndim)
+
+arr = np.array([1,2,3,4])
+print(arr)
+print(arr.ndim)
+
+mat = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(mat)
+print(mat.ndim)
